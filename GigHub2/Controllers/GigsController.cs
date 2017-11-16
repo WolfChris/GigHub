374c21daa02850fd3c3b05cdd84c6a2f53c5f421
Chris Wolf <chriswolf70@hotmail.com>
2017-11-16 15:22:39 +0100

    Refactoring of gig controller

--- GigHub2/Controllers/GigsController.cs
+++ GigHub2/Controllers/GigsController.cs
@@ -1,5 +1,7 @@
 ﻿using GigHub2.Models;
 using GigHub2.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -13,6 +15,7 @@ namespace GigHub2.Controllers
             _context = new ApplicationDbContext();
         }
 
+        [Authorize]
         public ActionResult Create()
         {
             var viewModel = new GigFormViewModel
@@ -20,6 +23,28 @@ namespace GigHub2.Controllers
                 Genres = _context.Genres.ToList()
             };
             return View(viewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(GigFormViewModel viewModel)
+        {
+            //var artistId = User.Identity.GetUserId();
+            //var artist = _context.Users.Single(u => u.Id == User.Identity.GetUserId());
+            //var genre = _context.Genres.Single(g => g.Id == viewModel.Genre);
+
+            var gig = new Gig
+            {
+                ArtistId = User.Identity.GetUserId(),
+                DateTime = DateTime.Parse(string.Format("{0} {1}", viewModel.Date, viewModel.Time)),
+                GenreId = viewModel.Genre,
+                Venue = viewModel.Venue
+            };
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+
         }
     }
 }
