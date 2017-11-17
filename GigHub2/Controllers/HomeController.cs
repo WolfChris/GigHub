c1eb5bf56ae0187190af7bc361bb297a5effcc4e
Chris Wolf <chriswolf70@hotmail.com>
2017-11-17 14:09:09 +0100

    Added name to appuser and updated db

--- GigHub2/Controllers/HomeController.cs
+++ GigHub2/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GigHub2.Models;
+using System.Data.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,18 @@ namespace GigHub2.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public HomeController()
+        {
+            _context = new ApplicationDbContext();
+        }
         public ActionResult Index()
         {
-            return View();
+            var upcomingGigs = _context.Gigs
+                .Include(g => g.Artist)
+                .Where(g => g.DateTime >= DateTime.Now);
+            return View(upcomingGigs);
         }
 
         public ActionResult About()
