fd5b5bf1af85320b6ce1f88ff747f0c7d25433f2
Chris Wolf <chriswolf70@hotmail.com>
2017-11-17 09:50:12 +0100

    antiforgery tokens added to create form and gigcontroller

--- GigHub2/Controllers/GigsController.cs
+++ GigHub2/Controllers/GigsController.cs
@@ -27,6 +27,7 @@ namespace GigHub2.Controllers
 
         [Authorize]
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Create(GigFormViewModel viewModel)
         {
 
