8496bc9bd64ab57e8aeed1f5a93e4adfd7bd126e
Chris Wolf <chriswolf70@hotmail.com>
2017-11-17 16:43:44 +0100

    implemented going/not going use case

--- GigHub2/Controllers/AttendancesController.cs
+++ GigHub2/Controllers/AttendancesController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GigHub2.Models;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -7,7 +9,39 @@ using System.Web.Http;
 
 namespace GigHub2.Controllers
 {
+    public class AttendanceDto
+    {
+        public int GigId { get; set; }
+    }
+
+    [Authorize]
     public class AttendancesController : ApiController
     {
+        private ApplicationDbContext _context;
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend(AttendanceDto dto)
+        {
+            var userId = User.Identity.GetUserId();
+            //var exists = _context.Attendances.Any(a => a.AttendeeId == userId && a.GigId == gigId);
+            if (_context.Attendances.Any(a => a.AttendeeId == userId && a.GigId == dto.GigId))
+            {
+                return BadRequest("The attendance already exists.");
+            }
+            var attendance = new Attendance
+            {
+                GigId = dto.GigId,
+                AttendeeId = userId
+
+            };
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+            return Ok();
+
+        }
     }
 }
